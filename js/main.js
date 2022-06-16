ae1c7871908434d93b40a562e949ad3f7f0b05c2
Zuhurbek <zuhurbek470@gmail.com>
2022-06-16 17:47:01 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,53 @@
+1-MASALA
+function addition(a, b) {
+	return a + b;	
+}
+
+2-MASALA
+function convert(minutes) {
+	return minutes * 60;
+}
+
+3-MASALA
+function addition(num) {
+	return num + 1;
+}
+
+4-MASALA
+function triArea(base, height) {
+	return (base * height) / 2; 
+}
+
+5-MASALA
+function calcAge(age) {
+	return age * 365;
+}
+
+6-MASALA
+function remainder(x, y) {
+	return x % y;
+}
+
+7-MASALA 
+function footballPoints(wins, draws, losses) {
+	return wins * 3 + draws*1 + losses*0;
+}
+
+8-MASALA
+function points(twoPointers, threePointers) {
+	return twoPointers*2 + threePointers*3;
+}
+
+9-MASALA 
+function lessThan100(a, b) {
+	if ((a + b) < 100) {
+		return true;
+	} else {
+		return false;
+	}
+}
+
+10-MASALA 
+function animals(chickens, cows, pigs) {
+	return chickens*2 + cows*4 + pigs*4;
+}
